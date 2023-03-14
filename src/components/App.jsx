68684e6a9cc8db2edf0253ea095465d655fc630b
VickyBondarenko <quikyv@gmail.com>
2023-03-14 21:00:56 +0100

    Change app component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,85 +1,66 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { List } from 'components/list/List';
 import Form from 'components/form/Form';
 import { Filter } from 'components/filter/Filter';
 import styled from 'styled-components';
 
 const CONTACTS_KEY = 'contacts';
-export class App extends Component {
-  state = {
-    contacts: [],
-    filter: '',
+export const App = () => {
+  const [contacts, setContacts] = useState([]);
+  const [filter, setFilter] = useState('');
+
+  const handleSubmit = contact => {
+    setContacts(prevState => [...prevState, contact]);
   };
 
-  componentDidMount() {
-    const contacts = localStorage.getItem(CONTACTS_KEY);
-    if (contacts && JSON.parse(contacts).length) {
-      this.setState({
-        contacts: JSON.parse(contacts),
-      });
-    }
-  }
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.contacts.length !== this.state.contacts.length) {
-      localStorage.setItem(CONTACTS_KEY, JSON.stringify(this.state.contacts));
-    }
-  }
-
-  handleSubmit = contact => {
-    this.setState(prevState => ({
-      contacts: [...prevState.contacts, contact],
-    }));
+  const onChangeSearch = e => {
+    setFilter(e.target.value);
   };
 
-  onChangeSearch = e => {
-    this.setState(prevState => ({
-      filter: e.target.value,
-    }));
-  };
-
-  applayFilter = () => {
-    return this.state.contacts.filter(contact =>
-      contact.name.toLowerCase().includes(this.state.filter.toLowerCase())
+  const applayFilter = () => {
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
     );
   };
 
-  handleCheck = user => {
+  const handleCheck = user => {
     if (
-      this.state.contacts.some(
+      contacts.some(
         contact => contact.name.toLowerCase() === user.name.toLowerCase()
       )
     ) {
       alert(`${user.name} is alredy in contact`);
     } else {
-      this.handleSubmit(user);
+      handleSubmit(user);
     }
   };
 
-  handleDeleteUser = id => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(user => user.id !== id),
-    }));
+  const handleDeleteUser = id => {
+    setContacts(prevState => prevState.filter(user => user.id !== id));
   };
 
-  render() {
-    return (
-      <Wraper>
-        <h1>Phonebook</h1>
-        <Form onCheck={this.handleCheck} />
+  useEffect(() => {
+    const contacts = localStorage.getItem(CONTACTS_KEY);
+    if (contacts && JSON.parse(contacts).length) {
+      setContacts(JSON.parse(contacts));
+    }
+  }, []);
 
-        <h2>Contacts</h2>
-        <Filter
-          value={this.state.filter}
-          onChangeSearch={this.onChangeSearch}
-        />
-        <List
-          options={this.applayFilter()}
-          onDeleteUser={this.handleDeleteUser}
-        />
-      </Wraper>
-    );
-  }
-}
+  useEffect(() => {
+    localStorage.setItem(CONTACTS_KEY, JSON.stringify(contacts));
+  }, [contacts]);
+
+  return (
+    <Wraper>
+      <h1>Phonebook</h1>
+      <Form onCheck={handleCheck} />
+
+      <h2>Contacts</h2>
+      <Filter value={filter} onChangeSearch={onChangeSearch} />
+      <List options={applayFilter()} onDeleteUser={handleDeleteUser} />
+    </Wraper>
+  );
+};
 
 const Wraper = styled.div`
   margin-left: 50px;
